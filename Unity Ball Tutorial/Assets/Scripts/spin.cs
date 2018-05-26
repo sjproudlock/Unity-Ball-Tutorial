713b782d21b5a43c2983628f7b23d843cdac3e74
sjproudlock <stuart.proudlock@gmail.com>
2018-05-26 21:07:32 +0100

    Ball and pickups

---
+++ Unity Ball Tutorial/Assets/Scripts/spin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour {
+
+    public Vector3 spinSpeed;
+
+	// Update is called once per frame
+	void Update () {
+	
+        transform.Rotate(spinSpeed * Time.deltaTime);
+
+
+	}
+}
