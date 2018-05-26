29e59eb1ae2b6b877076ba70758bd83b81e30da4
sjproudlock <stuart.proudlock@gmail.com>
2018-05-26 20:22:18 +0100

    Ball movement

---
+++ Unity Ball Tutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public float speed; 
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3 (moveH,0.0f,moveV);
+
+        
+        rb.AddForce (movement * speed);
+
+    }
+}
